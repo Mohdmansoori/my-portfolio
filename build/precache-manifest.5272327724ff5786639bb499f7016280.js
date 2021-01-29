c7d9e0a30c7812000e1d5b34e6687e2e0c4fd50d
Mohdmansoori <mansoorianas498@gmail.com>
2021-01-29 22:57:48 +0530

    about us changes or fixed

---
+++ build/precache-manifest.5272327724ff5786639bb499f7016280.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fc3a6fb32e7143a3d9fe398fc8a1fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68aee6d6213b97df79f8",
+    "url": "/static/css/main.6492f9a2.chunk.css"
+  },
+  {
+    "revision": "d45321fdf86178a4fc62",
+    "url": "/static/js/2.64d1a690.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.64d1a690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68aee6d6213b97df79f8",
+    "url": "/static/js/main.b75b5b87.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b95a4eaa63b15c0185d64e749b96927b",
+    "url": "/static/media/AnasResume.b95a4eaa.pdf"
+  },
+  {
+    "revision": "f9de737f18535765b99b5f08a6d54c2b",
+    "url": "/static/media/facebook.f9de737f.svg"
+  },
+  {
+    "revision": "2c472603337cec187259ed6cd32b21e3",
+    "url": "/static/media/github.2c472603.svg"
+  },
+  {
+    "revision": "bb5903db7ccae981497f8565c90f801d",
+    "url": "/static/media/instagram.bb5903db.svg"
+  },
+  {
+    "revision": "a13311a36cde2cfa79bff078a24103bf",
+    "url": "/static/media/mightycoder.a13311a3.svg"
+  },
+  {
+    "revision": "9aa92c215ad62783f883c2067b42a719",
+    "url": "/static/media/pin.9aa92c21.svg"
+  },
+  {
+    "revision": "d8c6f55311b71e7f30c8d4727f929bd7",
+    "url": "/static/media/tie.d8c6f553.svg"
+  }
+]);
