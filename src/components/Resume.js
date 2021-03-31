c18554876d281290e651860150a96d2d77552007
Mohdmansoori <mansoorianas498@gmail.com>
2021-03-31 16:33:21 +0530

    5th

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Resume = () => {
     return (
-        <div>
-            this is resume page
-        </div>
+      <div className="container resume">
+         <div className="row">
+             <div className="col-lg-6 resume-card">
+                 <h4 className="resume-card__heading">
+                     Education
+                 </h4>
+                 <div className="resume-card__body">
+                     <h5 className="reume-card__title">
+                         Software Engineer
+                     </h5>
+                     <p className="resume-card__name">
+                        Academy Of Technology 
+                     </p>
+                 </div>
+             </div>
+         </div>
+      </div>
     )
 }
 
